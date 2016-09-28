e0baedefb1916135b57e22aad87db20d853ba776
Anan Lappikulthong <ananlappikulthong@anans-macbook-air.local>
2016-09-28 22:59:34 +0700

    Initial Commit

---
+++ My Fav Place/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Fav Place
+//
+//  Created by Anan Lappikulthong on 9/28/2559 BE.
+//  Copyright © 2559 Anan Lappikulthong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
