a734067ead4ddf5543daeee4e8f0d7f4e6b59a55
Anan Lappikulthong <ananlappikulthong@Anans-MacBook-Air.local>
2016-10-01 21:19:53 +0700

    add file fof my-fav-place

---
+++ My Fav Place/PhotoVC.swift
@@ -0,0 +1,50 @@
+//
+//  PhotoVC.swift
+//  My Fav Place
+//
+//  Created by Anan Lappikulthong on 9/28/2559 BE.
+//  Copyright © 2559 Anan Lappikulthong. All rights reserved.
+//
+
+import UIKit
+
+class PhotoVC: UIViewController {
+    
+    @IBOutlet weak var Image1: UIImageView!
+    @IBOutlet weak var Image2: UIImageView!
+    @IBOutlet weak var seaImg: UIImageView!
+    @IBOutlet weak var lomaImg: UIImageView!
+    @IBOutlet weak var wahgohImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Image1.clipsToBounds = true
+        Image2.clipsToBounds = true
+        seaImg.clipsToBounds = true
+        lomaImg.clipsToBounds = true
+        wahgohImg.clipsToBounds = true
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backPhotoPress(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ My Fav Place/button.swift
@@ -0,0 +1,17 @@
+//
+//  button.swift
+//  My Fav Place
+//
+//  Created by Anan Lappikulthong on 9/28/2559 BE.
+//  Copyright © 2559 Anan Lappikulthong. All rights reserved.
+//
+
+import UIKit
+
+class button: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+    }
+
+}
